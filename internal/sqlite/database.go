96215934914a2fbe822df2b3c648c3786225bc07
Kevin Cantwell <kevin.cantwell@gmail.com>
2020-11-12 20:34:56 -0500

    adds drop table support

--- internal/sqlite/database.go
+++ internal/sqlite/database.go
@@ -1,6 +1,7 @@
 package sqlite
 
 import (
+	"context"
 	stdsql "database/sql"
 	"encoding/json"
 	"fmt"
@@ -21,7 +22,7 @@ type Database struct {
 var (
 	_ sql.Database     = (*Database)(nil)
 	_ sql.TableCreator = (*Database)(nil)
-	// _ sql.TableDropper = (*Database)(nil)
+	_ sql.TableDropper = (*Database)(nil)
 	// _ sql.TableRenamer = (*Database)(nil)
 )
 
@@ -188,274 +189,288 @@ func (db *Database) GetTableNames(ctx *sql.Context) ([]string, error) {
 	return tables, nil
 }
 
-func (db *Database) CreateTable(ctx *sql.Context, name string, schema sql.Schema) (finalErr error) {
-	var (
-		defs []string
-		pks  []string
-	)
+func (db *Database) CreateTable(ctx *sql.Context, name string, schema sql.Schema) error {
+	return inTx(ctx, db.w, func(tx *stdsql.Tx) error {
+		var (
+			defs []string
+			pks  []string
+		)
 
-	txn, err := db.w.BeginTx(ctx, nil)
+		type columnDefinition struct {
+			Name         string
+			Type         string
+			Affinity     string
+			PK           bool
+			Nullable     bool
+			Comment      string
+			DefaultValue *string // formatted for CREATE TABLE syntax
+			NumUnsigned  *bool
+			NumLength    *int64
+			NumScale     *int64
+			TxtCharset   *string
+			TxtCollate   *string
+			EnumVals     string // json array of strings
+		}
+
+		for cid, col := range schema {
+
+			def := columnDefinition{
+				Name:     col.Name,
+				Type:     col.Type.Type().String(),
+				PK:       col.PrimaryKey,
+				Nullable: col.Nullable,
+				Comment:  col.Comment,
+			}
+
+			// TODO: These cases may overlap if the interface is not very specific.
+			//       You must switch on the base types, unfortunately.
+			switch t := col.Type.(type) {
+			case sql.EnumType:
+				charset := string(t.CharacterSet())
+				if len(charset) > 0 {
+					def.TxtCharset = &charset
+				}
+				collate := string(t.Collation())
+				if len(collate) > 0 {
+					def.TxtCollate = &collate
+				}
+				b, err := json.Marshal(t.Values())
+				if err != nil {
+					return err
+				}
+				def.EnumVals = string(b)
+
+				if col.Default != nil {
+					d, err := t.Convert(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%s", d)
+					def.DefaultValue = &val
+				}
+				def.Affinity = "TEXT"
+			case sql.StringType:
+				charset := string(t.CharacterSet())
+				if len(charset) > 0 {
+					def.TxtCharset = &charset
+				}
+				length := t.MaxCharacterLength()
+				def.NumLength = &length
+				collate := string(t.Collation())
+				if len(collate) > 0 {
+					def.TxtCollate = &collate
+				}
+				if col.Default != nil {
+					d, err := t.Convert(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%s", d)
+					def.DefaultValue = &val
+				}
+
+				def.Affinity = "TEXT"
+			case sql.NumberType:
+				unsigned := !t.IsSigned()
+				def.NumUnsigned = &unsigned
+				if col.Default != nil {
+					d, err := t.Convert(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%v", d)
+					def.DefaultValue = &val
+				}
+				switch t.Type() {
+				case query.Type_INT8,
+					query.Type_INT16,
+					query.Type_INT24,
+					query.Type_INT32,
+					query.Type_INT64,
+					query.Type_UINT8,
+					query.Type_UINT16,
+					query.Type_UINT32,
+					query.Type_UINT64:
+					def.Affinity = "INTEGER"
+				case query.Type_FLOAT32,
+					query.Type_FLOAT64:
+					def.Affinity = "REAL"
+				default:
+					def.Affinity = "NUMERIC"
+				}
+			case sql.DecimalType:
+				length := int64(t.Precision())
+				scale := int64(t.Scale())
+				def.NumLength = &length
+				def.NumScale = &scale
+				if col.Default != nil {
+					d, err := t.Convert(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%v", d)
+					def.DefaultValue = &val
+				}
+				def.Affinity = "NUMERIC"
+			case sql.DatetimeType:
+				if col.Default != nil {
+					d, err := t.ConvertWithoutRangeCheck(col.Default)
+					if err != nil {
+						return err
+					}
+					val := d.Format("2006-01-02 15:04:05.999")
+					def.DefaultValue = &val
+				}
+				def.Affinity = "TEXT" // Best known way to allow for fractional seconds in SQLite
+			case sql.TimeType:
+				if col.Default != nil {
+					d, err := t.Marshal(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%d", d)
+					def.DefaultValue = &val
+				}
+				def.Affinity = "INTEGER"
+			case sql.SetType:
+				charset := string(t.CharacterSet())
+				if len(charset) > 0 {
+					def.TxtCharset = &charset
+				}
+				collate := string(t.Collation())
+				if len(collate) > 0 {
+					def.TxtCollate = &collate
+				}
+				b, err := json.Marshal(t.Values())
+				if err != nil {
+					return err
+				}
+				def.EnumVals = string(b)
+
+				if col.Default != nil {
+					d, err := t.Convert(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%s", d)
+					def.DefaultValue = &val
+				}
+				def.Affinity = "TEXT"
+			case sql.BitType:
+				if col.Default != nil {
+					d, err := t.Convert(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%d", d)
+					def.DefaultValue = &val
+				}
+				def.Affinity = "INTEGER"
+
+			case sql.YearType:
+				if col.Default != nil {
+					d, err := t.Convert(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%d", d)
+					def.DefaultValue = &val
+				}
+				def.Affinity = "INTEGER"
+			case sql.NullType:
+				def.Affinity = "TEXT"
+			case sql.JsonType:
+				if col.Default != nil {
+					d, err := t.Convert(col.Default)
+					if err != nil {
+						return err
+					}
+					val := fmt.Sprintf("%s", d)
+					def.DefaultValue = &val
+				}
+				def.Affinity = "TEXT"
+
+			default:
+				panic("TODO")
+			}
+
+			// These strings are added to the CREATE TABLE statement
+			colDefClause := fmt.Sprintf("%s %s", def.Name, def.Affinity)
+			if def.DefaultValue != nil {
+				colDefClause += fmt.Sprintf(" DEFAULT %q", *def.DefaultValue)
+			}
+			defs = append(defs, colDefClause)
+			if def.PK {
+				pks = append(pks, def.Name)
+			}
+
+			// Track mysql-specific metadata for each column definition
+			tx.Exec(
+				`INSERT INTO mysqlite_table_schema (
+					source,
+					cid,
+					name,
+					type,
+					pk,
+					nullable,
+					dflt_value,
+					comment,
+					num_unsigned,
+					num_length,
+					num_scale,
+					txt_charset,
+					txt_collate,
+					enum_vals
+				) VALUES (
+					?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+				)`,
+				name,
+				cid,
+				def.Name,
+				def.Type,
+				def.PK,
+				def.Nullable,
+				def.DefaultValue,
+				def.Comment,
+				def.NumUnsigned,
+				def.NumLength,
+				def.NumScale,
+				def.TxtCharset,
+				def.TxtCollate,
+				def.EnumVals,
+			)
+		}
+
+		if len(pks) > 0 {
+			defs = append(defs, fmt.Sprintf("PRIMARY KEY (%s)", strings.Join(pks, ", ")))
+		}
+		if _, err := tx.Exec(fmt.Sprintf(`CREATE TABLE "%s" (%s)`, name, strings.Join(defs, ", "))); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func (db *Database) DropTable(ctx *sql.Context, name string) error {
+	return inTx(ctx, db.w, func(tx *stdsql.Tx) error {
+		if _, err := tx.Exec(`DROP TABLE "` + name + `"`); err != nil {
+			return err
+		}
+		if _, err := tx.Exec(`DELETE FROM mysqlite_table_schema WHERE source = "` + name + `"`); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func inTx(ctx context.Context, db *stdsql.DB, f func(tx *stdsql.Tx) error) error {
+	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
-	defer func() {
-		if finalErr != nil {
-			_ = txn.Rollback()
-		} else {
-			finalErr = txn.Commit()
-		}
-	}()
-
-	type columnDefinition struct {
-		Name         string
-		Type         string
-		Affinity     string
-		PK           bool
-		Nullable     bool
-		Comment      string
-		DefaultValue *string // formatted for CREATE TABLE syntax
-		NumUnsigned  *bool
-		NumLength    *int64
-		NumScale     *int64
-		TxtCharset   *string
-		TxtCollate   *string
-		EnumVals     string // json array of strings
-	}
-
-	for cid, col := range schema {
-
-		def := columnDefinition{
-			Name:     col.Name,
-			Type:     col.Type.Type().String(),
-			PK:       col.PrimaryKey,
-			Nullable: col.Nullable,
-			Comment:  col.Comment,
-		}
-
-		// TODO: These cases may overlap if the interface is not very specific.
-		//       You must switch on the base types, unfortunately.
-		switch t := col.Type.(type) {
-		case sql.EnumType:
-			charset := string(t.CharacterSet())
-			if len(charset) > 0 {
-				def.TxtCharset = &charset
-			}
-			collate := string(t.Collation())
-			if len(collate) > 0 {
-				def.TxtCollate = &collate
-			}
-			b, err := json.Marshal(t.Values())
-			if err != nil {
-				return err
-			}
-			def.EnumVals = string(b)
-
-			if col.Default != nil {
-				d, err := t.Convert(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%s", d)
-				def.DefaultValue = &val
-			}
-			def.Affinity = "TEXT"
-		case sql.StringType:
-			charset := string(t.CharacterSet())
-			if len(charset) > 0 {
-				def.TxtCharset = &charset
-			}
-			length := t.MaxCharacterLength()
-			def.NumLength = &length
-			collate := string(t.Collation())
-			if len(collate) > 0 {
-				def.TxtCollate = &collate
-			}
-			if col.Default != nil {
-				d, err := t.Convert(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%s", d)
-				def.DefaultValue = &val
-			}
-
-			def.Affinity = "TEXT"
-		case sql.NumberType:
-			unsigned := !t.IsSigned()
-			def.NumUnsigned = &unsigned
-			if col.Default != nil {
-				d, err := t.Convert(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%v", d)
-				def.DefaultValue = &val
-			}
-			switch t.Type() {
-			case query.Type_INT8,
-				query.Type_INT16,
-				query.Type_INT24,
-				query.Type_INT32,
-				query.Type_INT64,
-				query.Type_UINT8,
-				query.Type_UINT16,
-				query.Type_UINT32,
-				query.Type_UINT64:
-				def.Affinity = "INTEGER"
-			case query.Type_FLOAT32,
-				query.Type_FLOAT64:
-				def.Affinity = "REAL"
-			default:
-				def.Affinity = "NUMERIC"
-			}
-		case sql.DecimalType:
-			length := int64(t.Precision())
-			scale := int64(t.Scale())
-			def.NumLength = &length
-			def.NumScale = &scale
-			if col.Default != nil {
-				d, err := t.Convert(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%v", d)
-				def.DefaultValue = &val
-			}
-			def.Affinity = "NUMERIC"
-		case sql.DatetimeType:
-			if col.Default != nil {
-				d, err := t.ConvertWithoutRangeCheck(col.Default)
-				if err != nil {
-					return err
-				}
-				val := d.Format("2006-01-02 15:04:05.999")
-				def.DefaultValue = &val
-			}
-			def.Affinity = "TEXT" // Best known way to allow for fractional seconds in SQLite
-		case sql.TimeType:
-			if col.Default != nil {
-				d, err := t.Marshal(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%d", d)
-				def.DefaultValue = &val
-			}
-			def.Affinity = "INTEGER"
-		case sql.SetType:
-			charset := string(t.CharacterSet())
-			if len(charset) > 0 {
-				def.TxtCharset = &charset
-			}
-			collate := string(t.Collation())
-			if len(collate) > 0 {
-				def.TxtCollate = &collate
-			}
-			b, err := json.Marshal(t.Values())
-			if err != nil {
-				return err
-			}
-			def.EnumVals = string(b)
-
-			if col.Default != nil {
-				d, err := t.Convert(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%s", d)
-				def.DefaultValue = &val
-			}
-			def.Affinity = "TEXT"
-		case sql.BitType:
-			if col.Default != nil {
-				d, err := t.Convert(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%d", d)
-				def.DefaultValue = &val
-			}
-			def.Affinity = "INTEGER"
-
-		case sql.YearType:
-			if col.Default != nil {
-				d, err := t.Convert(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%d", d)
-				def.DefaultValue = &val
-			}
-			def.Affinity = "INTEGER"
-		case sql.NullType:
-			def.Affinity = "TEXT"
-		case sql.JsonType:
-			if col.Default != nil {
-				d, err := t.Convert(col.Default)
-				if err != nil {
-					return err
-				}
-				val := fmt.Sprintf("%s", d)
-				def.DefaultValue = &val
-			}
-			def.Affinity = "TEXT"
-
-		default:
-			panic("TODO")
-		}
-
-		// These strings are added to the CREATE TABLE statement
-		colDefClause := fmt.Sprintf("%s %s", def.Name, def.Affinity)
-		if def.DefaultValue != nil {
-			colDefClause += fmt.Sprintf(" DEFAULT %q", *def.DefaultValue)
-		}
-		defs = append(defs, colDefClause)
-		if def.PK {
-			pks = append(pks, def.Name)
-		}
-
-		// Track mysql-specific metadata for each column definition
-		txn.Exec(
-			`INSERT INTO mysqlite_table_schema (
-				source,
-				cid,
-				name,
-				type,
-				pk,
-				nullable,
-				dflt_value,
-				comment,
-				num_unsigned,
-				num_length,
-				num_scale,
-				txt_charset,
-				txt_collate,
-				enum_vals
-			) VALUES (
-				?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
-			)`,
-			name,
-			cid,
-			def.Name,
-			def.Type,
-			def.PK,
-			def.Nullable,
-			def.DefaultValue,
-			def.Comment,
-			def.NumUnsigned,
-			def.NumLength,
-			def.NumScale,
-			def.TxtCharset,
-			def.TxtCollate,
-			def.EnumVals,
-		)
-	}
-
-	if len(pks) > 0 {
-		defs = append(defs, fmt.Sprintf("PRIMARY KEY (%s)", strings.Join(pks, ", ")))
-	}
-	if _, err := txn.Exec(fmt.Sprintf(`CREATE TABLE "%s" (%s)`, name, strings.Join(defs, ", "))); err != nil {
+	if err := f(tx); err != nil {
+		_ = tx.Rollback()
 		return err
 	}
-	return nil
+	return tx.Commit()
 }
